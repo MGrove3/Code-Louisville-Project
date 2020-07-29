579d975019d9c48ff9be6ff66b08a71b41aa6e52
Matt Grove <MGrove@Fox41.com>
2020-07-29 07:26:13 -0400

    eigth commit

--- javascript.js
+++ javascript.js
@@ -1,8 +1,15 @@
-history.pushState(null, null, document.title);
-window.addEventListener('popstate', function () {
-    history.pushState(null, null, document.title);
-});
 
+/* Create a JavaScript function whose return value is used in your site. The function must be triggered by user action (ex: clicking a button). */
 function goToHome () { 
-    window.location.href = "index.html"
+    window.location.href = "index.html";
 };
+
+
+/* Use JavaScript to perform math operations and display the result on your site */
+alert(30 * 42);
+/* Equal to 1260 */
+alert(12 + 56);
+/* Equal to 68 */
+alert(33 / 2.5);
+/* Equal to 13.2 */
+
