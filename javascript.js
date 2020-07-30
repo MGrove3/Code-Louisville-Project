a0e5e374304924e9a73fc235b0252189bf433f21
Matt Grove <MGrove@Fox41.com>
2020-07-30 15:54:20 -0400

    16th commit

--- javascript.js
+++ javascript.js
@@ -21,3 +21,31 @@ var y = 4
 var z = x * y - w
 alert(z)
 /* Equals to 162 */
+
+/* Slider Javascript */
+var slider_content = document.getElementById('box');
+
+/* Containing the images in an array */
+var image = ['a', 'b', 'c'];
+
+var i = image.length;
+
+// function for next slide
+function nextImage(){
+    if (i<image.length) {
+        i= image.length;
+    }else{
+        i=1;
+    }
+        slider_content.innerHTML = "<img src="+image[i-1]+".jpg>";
+}
+
+//function for previous slide
+function prevImage() {
+    if (i<image.length+1 && i>1) {
+        i=image.length;
+    }else{
+        i=1;
+    }
+        slider_content.innerHTML = "<img src="+image[i-1]+".jpg>";
+}
