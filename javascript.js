fc90df3929767088a2d7648d64aea0230833ccca
Matt Grove <MGrove@Fox41.com>
2020-07-31 00:02:54 -0400

    17th and final commit

--- javascript.js
+++ javascript.js
@@ -4,48 +4,22 @@ function goToHome () {
     window.location.href = "index.html";
 };
 
+/*math equation function */
+var x = 23;
+var y = 187;
+var z = 122;
 
-/* Use JavaScript to perform math operations and display the result on your site */
-alert(30 * 42);
-/* Equal to 1260 */
-alert(12 + 56);
-/* Equal to 68 */
-alert(33 / 2.5);
-/* Equal to 13.2 */
+var w = z + 4 * ( y - x)
+console.log (w)
+/* Answer is 778 */
 
-/* A math equation instead of just an alert */
-var w = 10
-var x = 43
-var y = 4
-
-var z = x * y - w
-alert(z)
-/* Equals to 162 */
-
-/* Slider Javascript */
-var slider_content = document.getElementById('box');
-
-/* Containing the images in an array */
-var image = ['a', 'b', 'c'];
-
-var i = image.length;
-
-// function for next slide
-function nextImage(){
-    if (i<image.length) {
-        i= image.length;
-    }else{
-        i=1;
-    }
-        slider_content.innerHTML = "<img src="+image[i-1]+".jpg>";
-}
-
-//function for previous slide
-function prevImage() {
-    if (i<image.length+1 && i>1) {
-        i=image.length;
-    }else{
-        i=1;
-    }
-        slider_content.innerHTML = "<img src="+image[i-1]+".jpg>";
-}
+/* Function to generate random number */ 
+function randomNumber(min, max) {  
+    min = Math.ceil(min); 
+    max = Math.floor(max); 
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+}  
+ console.log("Random Number between 1 and 10: ")  
+  
+/* Function call */
+console.log( randomNumber(1, 10) );
